c021dd3b7050211686f06144d49d5924a6207169
abhishekasthana01 <88158569+abhishekasthana01@users.noreply.github.com>
2025-09-23 21:57:34 +0530

    plugin

--- Beam-CMS/config/plugins.js
+++ Beam-CMS/config/plugins.js
@@ -3,16 +3,18 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        baseUrl: env('AWS_CLOUDFRONT_URL') || `https://${env('AWS_BUCKET_NAME')}.s3.${env('AWS_REGION')}.amazonaws.com`,
-        rootPath: env('AWS_S3_ROOT_PATH') || '',
         s3Options: {
           accessKeyId: env('AWS_ACCESS_KEY_ID'),
           secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
           region: env('AWS_REGION'),
+          // Include baseUrl and rootPath here to avoid deprecation warning
+          baseUrl:
+            env('AWS_CLOUDFRONT_URL') ||
+            `https://${env('AWS_BUCKET_NAME')}.s3.${env('AWS_REGION')}.amazonaws.com`,
+          rootPath: env('AWS_S3_ROOT_PATH') || '',
         },
         params: {
           Bucket: env('AWS_BUCKET_NAME'),
-          ACL: null,
         },
       },
     },
@@ -24,8 +26,14 @@ module.exports = ({ env }) => ({
         apiKey: env('SENDGRID_API_KEY'),
       },
       settings: {
-        defaultFrom: env('SENDGRID_DEFAULT_FROM', 'mirassol.pedro7@gmail.com'),
-        defaultReplyTo: env('SENDGRID_DEFAULT_REPLY_TO', 'mirassol.pedro7@gmail.com'),
+        defaultFrom: env(
+          'SENDGRID_DEFAULT_FROM',
+          'mirassol.pedro7@gmail.com'
+        ),
+        defaultReplyTo: env(
+          'SENDGRID_DEFAULT_REPLY_TO',
+          'mirassol.pedro7@gmail.com'
+        ),
       },
     },
   },
