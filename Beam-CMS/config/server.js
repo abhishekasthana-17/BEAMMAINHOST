970ee655ea42ddf9b80b48ae81e888fdbfb7ad17
abhishekasthana01 <88158569+abhishekasthana01@users.noreply.github.com>
2025-09-23 23:31:01 +0530

    Fix Render deployment issues: remove .DS_Store files and update .env for SQLite

--- Beam-CMS/config/server.js
+++ Beam-CMS/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 10000),
+  port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', ''),
   app: {
     keys: env.array('APP_KEYS', ['default-key-1', 'default-key-2']),
